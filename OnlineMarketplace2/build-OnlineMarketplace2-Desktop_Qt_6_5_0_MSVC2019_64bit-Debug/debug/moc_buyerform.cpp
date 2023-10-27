36ebcd354f8b39f9b8718bf0e4ab8b188cac4cf4
Aly Yehia Zakaria <110777669+AlyYehiaZak@users.noreply.github.com>
2023-10-27 17:10:55 +0200

    Add files via upload

---
+++ OnlineMarketplace2/build-OnlineMarketplace2-Desktop_Qt_6_5_0_MSVC2019_64bit-Debug/debug/moc_buyerform.cpp
@@ -0,0 +1,237 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'buyerform.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../OnlineMarketplace2/buyerform.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'buyerform.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSbuyerformENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSbuyerformENDCLASS = QtMocHelpers::stringData(
+    "buyerform",
+    "on_pushButton_show_clicked",
+    "",
+    "on_pushButton_logout_clicked",
+    "on_pushButton_showbycategory_clicked",
+    "on_pushButton_search_clicked",
+    "on_pushButton_rate_clicked",
+    "on_pushButton_showbyrating_clicked",
+    "on_pushButton_addCart_clicked",
+    "on_pushButton_view_cart_clicked",
+    "on_pushButton_remove_clicked",
+    "on_pushButton_confirm_clicked",
+    "on_pushButton_clicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSbuyerformENDCLASS_t {
+    uint offsetsAndSizes[26];
+    char stringdata0[10];
+    char stringdata1[27];
+    char stringdata2[1];
+    char stringdata3[29];
+    char stringdata4[37];
+    char stringdata5[29];
+    char stringdata6[27];
+    char stringdata7[35];
+    char stringdata8[30];
+    char stringdata9[32];
+    char stringdata10[29];
+    char stringdata11[30];
+    char stringdata12[22];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSbuyerformENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSbuyerformENDCLASS_t qt_meta_stringdata_CLASSbuyerformENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 9),  // "buyerform"
+        QT_MOC_LITERAL(10, 26),  // "on_pushButton_show_clicked"
+        QT_MOC_LITERAL(37, 0),  // ""
+        QT_MOC_LITERAL(38, 28),  // "on_pushButton_logout_clicked"
+        QT_MOC_LITERAL(67, 36),  // "on_pushButton_showbycategory_..."
+        QT_MOC_LITERAL(104, 28),  // "on_pushButton_search_clicked"
+        QT_MOC_LITERAL(133, 26),  // "on_pushButton_rate_clicked"
+        QT_MOC_LITERAL(160, 34),  // "on_pushButton_showbyrating_cl..."
+        QT_MOC_LITERAL(195, 29),  // "on_pushButton_addCart_clicked"
+        QT_MOC_LITERAL(225, 31),  // "on_pushButton_view_cart_clicked"
+        QT_MOC_LITERAL(257, 28),  // "on_pushButton_remove_clicked"
+        QT_MOC_LITERAL(286, 29),  // "on_pushButton_confirm_clicked"
+        QT_MOC_LITERAL(316, 21)   // "on_pushButton_clicked"
+    },
+    "buyerform",
+    "on_pushButton_show_clicked",
+    "",
+    "on_pushButton_logout_clicked",
+    "on_pushButton_showbycategory_clicked",
+    "on_pushButton_search_clicked",
+    "on_pushButton_rate_clicked",
+    "on_pushButton_showbyrating_clicked",
+    "on_pushButton_addCart_clicked",
+    "on_pushButton_view_cart_clicked",
+    "on_pushButton_remove_clicked",
+    "on_pushButton_confirm_clicked",
+    "on_pushButton_clicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSbuyerformENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      11,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   80,    2, 0x08,    1 /* Private */,
+       3,    0,   81,    2, 0x08,    2 /* Private */,
+       4,    0,   82,    2, 0x08,    3 /* Private */,
+       5,    0,   83,    2, 0x08,    4 /* Private */,
+       6,    0,   84,    2, 0x08,    5 /* Private */,
+       7,    0,   85,    2, 0x08,    6 /* Private */,
+       8,    0,   86,    2, 0x08,    7 /* Private */,
+       9,    0,   87,    2, 0x08,    8 /* Private */,
+      10,    0,   88,    2, 0x08,    9 /* Private */,
+      11,    0,   89,    2, 0x08,   10 /* Private */,
+      12,    0,   90,    2, 0x08,   11 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject buyerform::staticMetaObject = { {
+    QMetaObject::SuperData::link<QDialog::staticMetaObject>(),
+    qt_meta_stringdata_CLASSbuyerformENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSbuyerformENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSbuyerformENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<buyerform, std::true_type>,
+        // method 'on_pushButton_show_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_logout_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_showbycategory_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_search_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_rate_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_showbyrating_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_addCart_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_view_cart_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_remove_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_confirm_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void buyerform::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<buyerform *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_pushButton_show_clicked(); break;
+        case 1: _t->on_pushButton_logout_clicked(); break;
+        case 2: _t->on_pushButton_showbycategory_clicked(); break;
+        case 3: _t->on_pushButton_search_clicked(); break;
+        case 4: _t->on_pushButton_rate_clicked(); break;
+        case 5: _t->on_pushButton_showbyrating_clicked(); break;
+        case 6: _t->on_pushButton_addCart_clicked(); break;
+        case 7: _t->on_pushButton_view_cart_clicked(); break;
+        case 8: _t->on_pushButton_remove_clicked(); break;
+        case 9: _t->on_pushButton_confirm_clicked(); break;
+        case 10: _t->on_pushButton_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *buyerform::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *buyerform::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSbuyerformENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int buyerform::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 11)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 11;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 11)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 11;
+    }
+    return _id;
+}
+QT_WARNING_POP
